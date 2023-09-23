b43213690da74f0765218b97e962cd46a0f85809
ediva <edivaldoestudos@gmail.com>
2023-09-23 13:02:53 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/appsorteio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.appsorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+  @SuppressLint("SetTextI18n")
+  public void sortearNumero(View view){
+      TextView  texto = findViewById(R.id.nmSelecionado);
+      int nM = new Random().nextInt(26);
+      texto.setText("Número: " + nM);
+
+
+  }
+
+}
